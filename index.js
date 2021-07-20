f20553e161eeeb69112862889a20705fa23c08bd
BeTr4y4l <75473312+BeTr4y4l@users.noreply.github.com>
2021-07-20 12:06:05 +0200

    second commit

--- index.js
+++ index.js
@@ -21,14 +21,36 @@ app.get("/task.todo", (req, res) => {
 	const task = tasks.filter((task) => task.checked === false);
 	res.status(200).json(task);
 });
+// Get tâche checkées (faites / complétées)
+app.get("/task.done", (req, res) => {
+	console.log("ici");
+	const task = tasks.filter((task) => task.checked === true);
+	res.status(200).json(task);
+});
+
+// Ajout d'une tâche
+app.post("/task", (req, res) => {
+	tasks.push(req.body);
+	res.status(200).json(tasks);
+});
 
 // Modification d'une tâche
-app.put("/task/:id", (req, res) => {
+app.patch("/task/:id", (req, res) => {
 	const id = parseInt(req.params.id);
 	let task = tasks.find((task) => task.id === id);
 	(task.title = req.body.title),
 		(task.content = req.body.content),
+		(task.checked = req.body.checked),
+		(task.image = req.body.image),
 		res.status(200).json(task);
+});
+
+// Suppression d'une tâche
+app.delete("/task/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	let task = tasks.find((task) => task.id === id);
+	tasks.splice(tasks.indexOf(task), 1);
+	res.status(200).json(tasks);
 });
 
 // Serveur lancé
